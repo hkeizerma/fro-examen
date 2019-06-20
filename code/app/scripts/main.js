ddc185fbe2c44eaf317bd59da60ebe28ee2d7e47
Quinten <Quintenr11.qq@gmail.com>
2019-06-20 16:03:28 +0200

    overhaul met modaal(details moeten nog afgemaakt)

--- code/app/scripts/main.js
+++ code/app/scripts/main.js
@@ -1,9 +1,39 @@
-console.log('\'Allo \'Allo!');
+let modal1 = document.getElementById('regModal');
+let btn1 = document.getElementById('regBtn');
+let span1 = document.getElementsByClassName('close')[0];
 
-// Uncomment to enable Bootstrap tooltips
-// https://getbootstrap.com/docs/4.0/components/tooltips/#example-enable-tooltips-everywhere
-// $(function () { $('[data-toggle="tooltip"]').tooltip(); });
+console.log('hoi');
 
-// Uncomment to enable Bootstrap popovers
-// https://getbootstrap.com/docs/4.0/components/popovers/#example-enable-popovers-everywhere
-// $(function () { $('[data-toggle="popover"]').popover(); });
+btn1.onclick = function () {
+    modal1.style.display = 'block';
+};
+
+span1.onclick = function () {
+    modal1.style.display = 'none';
+};
+
+window.onclick = function(event) {
+    if (event.target === modal1) {
+        modal1.style.display = 'none';
+    }
+};
+
+let modal2 = document.getElementById('loginModal');
+let btn2 = document.getElementById('loginBtn');
+let span2 = document.getElementsByClassName('close')[1];
+
+console.log('hoi');
+
+btn2.onclick = function () {
+    modal2.style.display = 'block';
+};
+
+span2.onclick = function () {
+    modal2.style.display = 'none';
+};
+
+window.onclick = function(event) {
+    if (event.target === modal2) {
+        modal2.style.display = 'none';
+    }
+};
